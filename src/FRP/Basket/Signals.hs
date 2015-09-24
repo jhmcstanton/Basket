ecb189a784f18fd484cdf6acd73de4a31d706c89
Jim Stanton <jim@jhstanton.com>
2015-09-24 10:36:10 -0500

    Added additional regulator

--- src/FRP/Basket/Signals.hs
+++ src/FRP/Basket/Signals.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeOperators, DataKinds, 
              ScopedTypeVariables, TypeFamilies, FlexibleContexts,
-             FlexibleInstances #-}
+             FlexibleInstances, UndecidableInstances #-}
 
 module FRP.Basket.Signals where
 
@@ -12,14 +12,13 @@ import Control.Arrow
 import Data.Monoid hiding ((<>))
 import Data.Semigroup
 --import FRP.Basket.Aux.HList
-import Data.HList hiding ((#))
+import Data.HList 
 
 type Time = Double
 
 newtype Signal s a b = Signal {
                            runSignal :: Time -> HList s -> a -> (b, HList s)
                        }
-
 
 
 mkSignal :: (Time -> s -> a -> (b, s)) -> Signal '[s] a b
@@ -31,6 +30,7 @@ liftS :: (a -> b) -> Signal '[] a b
 liftS f = Signal $ \_ s a -> (f a, s)
 
 -- Pronounced 'weave', this function composes Signals of differing states 
+infixr #>
 (#>) :: forall s s' ss a b c n. (HSplitAt n ss s s', ss ~ HAppendListR s (HAppendListR s' '[]), 
                                 HAppendFD s' '[] s', HAppendFD s s' ss) => 
        Signal s  a b -> Signal s' b c -> Signal ss a c
@@ -52,7 +52,7 @@ instance Functor (Signal s a) where
 
 
 instance Applicative (Signal s a) where
-  pure a = Signal $ \_ x _ -> (a, x)
+  pure a = Signal $ \_ s _ -> (a, s)
   (Signal f) <*> (Signal g) = Signal $ \t s a -> let (b, s' ) = g t s a 
                                                      (h, s'') = f t s' a in (h b, s'')
 
@@ -74,17 +74,19 @@ instance Monoid (Signal s a a) where
   mempty = Signal $ \_ s a -> (a, s)
   mappend = (<>)
 
--- state doesn't really compose in a reasonable way for this to actually be an arrow..
-instance Arrow (Signal s) where
+
+-- Maybe a little dissapointing that this needs to be constrained this way, but otherwise
+-- the final state is not clear.  
+instance Monoid (HList s) => Arrow (Signal s) where
   arr f = Signal $ \_ s a -> (f a, s)
   first (Signal f) = Signal $ \t s (a, c) -> let (b, s') = f t s a in ((b, c), s')
   second (Signal f) = Signal $ \t s (c, a) -> let (b, s') = f t s a in ((c, b), s')
   (Signal f) *** (Signal g) = Signal $ \t s (a, c) -> let (b, s' ) = f t s a 
-                                                          (d, s'') = g t s c in ((b, d), s) -- which s to use ?
+                                                          (d, s'') = g t s c in ((b, d), s' `mappend` s'') -- which s to use ?
   (Signal f) &&& (Signal g) = Signal $ \t s a -> let (b, s') = f t s a 
-                                                     (d, s'') = g t s a in ((b, d), s) -- which s to use ?
+                                                     (d, s'') = g t s a in ((b, d), s' `mappend` s'') -- which s to use ?
 
-instance ArrowChoice (Signal s) where
+instance Monoid (HList s) => ArrowChoice (Signal s) where
   left (Signal f) = Signal $ \t s e -> case e of
                                          Left b -> let (c, s') = f t s b in (Left c, s')
                                          Right d -> (Right d, s)
@@ -93,11 +95,8 @@ instance ArrowChoice (Signal s) where
                                          Right d -> let (b, s') = f t s d in (Right b, s')
 
 
-instance ArrowApply (Signal s) where
+instance Monoid (HList s) => ArrowApply (Signal s) where
   app = Signal $ \t s (sf, b) -> runSignal sf t s b
 
-instance ArrowLoop (Signal s) where
+instance Monoid (HList s) => ArrowLoop (Signal s) where
   loop (Signal f) = Signal $ \t s a -> let ((b, d), s') = f t s (a, d) in (b, s')
-
-
-
--- src/FRP/Basket/Signals/Switches.hs
+++ src/FRP/Basket/Signals/Switches.hs
@@ -12,11 +12,11 @@ data Event e = Event e | NoEvent deriving (Show, Eq)
 {- | Whenever the signal e occurs the handler function is called for the return value.
      The handler and the signal have differing states and the switch keeps track of both. 
 -}
-switch :: forall s s' ss a b e n. 
+hSwitch :: forall s s' ss a b e n. 
           (HSplitAt n ss s s', ss ~ HAppendListR s (HAppendListR s' '[]), 
            HAppendFD s' '[] s', HAppendFD s s' ss) => 
           Signal s a (b, Event e) -> Signal s' e b -> Signal ss a b
-switch sf handler = Signal $ \t totalState a -> 
+hSwitch sf handler = Signal $ \t totalState a -> 
                                let splitIndex        = Proxy :: Proxy n
                                    (sfst, handlerst) =  hSplitAt splitIndex totalState
                                    ((b, ev), sfst')  = runSignal sf t sfst a
@@ -29,8 +29,8 @@ switch sf handler = Signal $ \t totalState a ->
      with differing state types, these are equivalent. In this case the state that the handler uses
      is the returned state of the original function call (sf) 
 -}
-monoSwitch :: Signal s a (b, Event e) -> Signal s e b -> Signal s a b
-monoSwitch sf handler = Signal $ \t s a -> 
+switch :: Signal s a (b, Event e) -> Signal s e b -> Signal s a b
+switch sf handler = Signal $ \t s a -> 
                                    let ((b, ev), s') = runSignal sf t s a
                                    in case ev of 
                                         NoEvent -> (b, s')
@@ -38,24 +38,32 @@ monoSwitch sf handler = Signal $ \t s a ->
 
 
 {- | This function allows a 'regulator' to see the output of a signal along with it's 
-     internal state to perform some time of regulatory transformation on its output.
+     internal state to perform some time of regulatory transformation on its output.     
 -}
 
-regulate :: forall s s' ss a b c n. 
+hRegulator :: forall s s' ss a b c n. 
             (HSplitAt n ss s s', 
              ss ~ HAppendListR s (HAppendListR s' '[]), 
              HAppendFD s' '[] s', HAppendFD s s' ss) =>
              Signal s a b -> Signal s' (b, HList s) c -> Signal ss a c
-regulate sf regulator = 
+hRegulator sf regulator = 
   Signal $ \t totalState a -> let splitIndex        = Proxy :: Proxy n
                                   (sfState, rState) = hSplitAt splitIndex totalState
                                   (b, sfState')     = runSignal sf t sfState a
                                   (c, rState')      = runSignal regulator t rState (b, sfState')
                               in (c, hConcat $ hBuild sfState' rState')
 
+{- |
+  A regulator that compares the initial state of the Signal sf it regulates to sf's output
+-}
+regulator :: Signal s a b -> Signal s (b, HList s) c -> Signal s a c
+regulator sf regulator = 
+  Signal $ \t s a -> let (b, s') = runSignal sf t s a 
+                     in runSignal regulator t s (b, s')
 
-monoPSwitch :: Signal s a (Either b c) -> Signal s b d -> Signal s c d -> Signal s a d
-monoPSwitch main left right = 
+
+parSwitch :: Signal s a (Either b c) -> Signal s b d -> Signal s c d -> Signal s a d
+parSwitch main left right = 
   Signal $ \t s a -> let (r, s') = runSignal main t s a in
                      case r of
                        Left  b  -> runSignal left t s' b
